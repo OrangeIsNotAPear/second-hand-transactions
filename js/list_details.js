0e3cb72eb83d79dcc7df74eaaa0e18ef10ac72bf
orange <orange@qq.com>
2019-07-18 20:30:37 +0800

    添加登录页面及js

--- js/list_details.js
+++ js/list_details.js
@@ -1,13 +1,16 @@
-(function(){
-    var as=document.querySelectorAll("#card_ul>li[data-toggle=card_ul]")
-    var i=10;
-    for(var a of as){
-        a.onclick=function(){
-            var id=this.getAttribute("data-target");
-            var div=document.getElementById(id);
-            div.style.zIndex=i++;
-            div.style.background = "#0a8494";
-            div.style.color = "white";
-        }
+/* 留言框部分 */
+// 将事件绑定在父元素ul上
+$("ul.card_ul").click(function(e){
+    e.preventDefault();
+    //用e.target代替this获得目标元素
+    var $a=$(e.target);
+    //只有$a有data-toggle属性且属性值为tab时
+    if($a.is("[data-toggle=tab]")==true){
+      //才给当前$a的爹加active
+      $a.parent().addClass("active")
+      //return $a.parent()
+      //然后给$a的爹的其余兄弟们都去掉active
+      .siblings().removeClass("active");
     }
-})()
+    $("div.content1").addClass("active").siblings().removeClass("active");
+  })
